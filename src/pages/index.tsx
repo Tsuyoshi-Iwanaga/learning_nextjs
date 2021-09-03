b336d3daaee71b90ed058c9d95755ef66c252f97
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-09-03 09:57:46 +0900

    add styled-components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import Head from 'next/head'
+import styled from 'styled-components'
 // import styles from '../../styles/Home.module.css'
 
 export default function Home() {
+  const HeadingH1 = styled.h1`
+    font-size: 16px;
+    color: blue;
+  `
   return (
-    <h1>index</h1>
+    <HeadingH1>Index</HeadingH1>
   )
 }
