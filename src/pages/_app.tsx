c9a967e1dc58de727eb7686a292009645d59ff7e
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-09-19 23:05:39 +0900

    apply types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 // import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+import { AppProps } from 'next/app'
+
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
