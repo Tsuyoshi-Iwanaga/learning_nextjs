e8ca931e88a2adf994056ba6ca9d84d584f18053
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-09-20 22:33:16 +0900

    use Head Component from next

--- src/pages/posts/first-post.tsx
+++ src/pages/posts/first-post.tsx
@@ -1,14 +1,18 @@
+import Head from 'next/head'
 import Link from 'next/link'
 
 export default function FirstPost() {
   return (
     <>
-    <h1>FirstPost</h1>
-    <h2>
-      <Link href="/">
-        <a>Back to Top</a>
-      </Link>
-    </h2>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>FirstPost</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to Top</a>
+        </Link>
+      </h2>
     </>
   )
 }
