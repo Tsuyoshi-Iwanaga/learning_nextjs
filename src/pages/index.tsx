0155b97d00b443cbe1846194bbe045a70ab910ef
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-09-22 00:39:43 +0900

    fetch external data & display to top page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,9 @@ import Head from 'next/head'
 import styled from 'styled-components'
 import Layout, { siteTitle } from '@/components/layout'
 import utilStyles from '@/styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   const Introduce = styled.p`
     font-size: 24px;
     font-weight: bold;
@@ -17,6 +18,29 @@ export default function Home() {
         <Introduce>Hello I'm Tsuyoshi.<br />I'm workign frontend engineer in Japan.</Introduce>
         <p>(This is a sample website - you’ll be building a site like this on <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)</p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
